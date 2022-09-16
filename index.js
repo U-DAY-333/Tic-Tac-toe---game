006324ea5cd584b55e5790745a92ebe21099e63b
Shinagam sai uday kiran <93582768+U-DAY-333@users.noreply.github.com>
2022-09-16 11:03:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function clikcings()
         if(a == 'X') this.innerHTML = "<span style='color: red;'>" + a + "</span>";
         else this.innerHTML = a;
 
-         if(a == 'X') audio.play();
+        if(a == 'X') audio.play();
         else new Audio("yellow.mp3").play(); 
        
         if(a == 'X') 
